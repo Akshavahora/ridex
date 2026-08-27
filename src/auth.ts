7e9725da7fd3331552fd9464bf2258bed9a43e66
Aksha Vahora <vahoraakshu@gmail.com>
2026-08-27 22:36:58 +0530

    Setting up Auth.js for authentication

---
+++ src/auth.ts
@@ -0,0 +1,124 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import connectDb from "./lib/db"
+import User from "./models/user.model";
+import bcrypt from "bcryptjs";
+import Google from "next-auth/providers/google";
+
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [
+        Credentials({ //object
+            credentials: {
+                email: {
+                    type: "email",
+                    label: "Email",
+                    placeholder: "johndoe@gmail.com",
+                },
+                password: {
+                    type: "password",
+                    label: "Password",
+                    placeholder: "*****",
+                },
+            },
+
+            async authorize(credentials, request) {
+                if (!credentials.email || !credentials.password) {
+                    throw Error("missing credentials");
+                }
+
+                const email = credentials.email;
+                const password = credentials.password as string;
+
+                await connectDb()
+
+                //  check if user is exist?, if not then do sign up
+                const user = await User.findOne({ email })
+                if (!user) {
+                    throw Error("User doesn't exist!");
+                }
+
+                if (!user.password) {
+                    throw Error("User doesn't have a password!");
+                }
+
+                // check password with database stored password
+                const isMatch = await bcrypt.compare(password, user.password)
+                if (!isMatch) {
+                    throw Error("Incorrect Password!");
+                }
+
+                return {
+                    id: user._id.toString(),
+                    name: user.name,
+                    email: user.email,
+                    role: user.role
+                }
+            }
+        }),
+
+        Google ({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET
+        })
+    ],
+
+    // When user login and logout what functions should be called is written here 
+    callbacks: {
+
+        async signIn({user, account}) {
+            if(account?.provider == "google") {
+                await connectDb()
+                const dbUser = await User.findOne({email: user.email})
+                if(!dbUser) {
+                    // if not found user then create user
+                    await User.create({
+                        name: user.name,
+                        email: user.email
+                    })
+                }
+
+                user.id=dbUser?._id;
+                user.role = dbUser?.role
+            }
+
+            return true
+        },
+
+        async jwt({ token, user }) {  //user is provided by auth.js 
+            if(user) {
+                token.name = user.name,
+                token.id = user.id,
+                token.email = user.email,
+                token.role = user.role
+            }
+            
+
+            return token
+        },
+
+        async session ({ token, session }) {
+
+            if (session.user) {
+                session.user.name = token.name,
+                session.user.id = token.id as string,
+                session.user.email = token.email as string,
+                session.user.role = token.role as string
+            }
+
+            return session
+        }
+    },
+
+    pages: {
+        signIn:"/signin",
+        error: "/signin",
+
+    },
+
+    session: {
+        strategy: "jwt",
+        maxAge: 10*24*60*60
+    },
+    secret: process.env.BETTER_AUTH_SECRET,
+})
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,6 +4,7 @@ interface IUser extends Document {
     name: string;
     email: string;
     password?: string;
+    role: "user" | "partner" | "admin";
     createdAt: Date;
     updatedAt: Date;
 }
@@ -12,7 +13,7 @@ const userSchema = new mongoose.Schema<IUser>({
     name: {
         type: String,
         required: true,
-    },
+    }, 
     email: {
         type: String,
         required: true,
@@ -21,8 +22,14 @@ const userSchema = new mongoose.Schema<IUser>({
     password: {
         type: String,
     },
+    role:{
+        type: String,
+        default: "user",
+        enum: ["user", "partner", "admin"]
+    }
+    
 },{timestamps: true});
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default  User;
